b8ad2e5192329f890e45349130b244b1ee04cd63
Markus Röhstö <markus.rohsto@gmail.com>
2021-02-01 12:03:20 +0100

    Update ntp_diag.sh
    Ny och bättre!!!

--- ntp_diag.sh
+++ ntp_diag.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
-#remote st delay offset jitter
-printf 'remote st delay offset jitter\n'
-printf '%s %s\n' "$(date)" "$line"; ntpq -p | grep -v "==" |grep -v "remote" | awk -F " " '{ print $1,$3,$8,$9,$10 }'
+
+SERVER="192.36.143.130"
+OUT="/tmp/timediff1.csv"
+
+get_ntp_time () {
+    printf "$(date +%Y-%m-%d), $(date +%T:%N), " >> $OUT
+    ntpdate -q $SERVER | grep -v "^server " | awk -F " " '{ print $1,$2" 2021,",$3",",$8",",$10 }'
+}
+
+log_to_file () {
+    if [ ! -f "$OUT" ]; then
+        #echo "$OUT does not exist."
+        touch $OUT
+        echo "Local Date, Local Timestamp, NTP-date, NTP-time, server, offset" > $OUT
+        get_ntp_time >> $OUT
+    else
+        #echo "$OUT exists."
+        get_ntp_time >> $OUT    
+    fi
+}
+
+while true; do log_to_file; sleep 2; done
