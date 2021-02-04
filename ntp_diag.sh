949fa93525d7315c3a11f18b167bdd964e3f9ed3
Marqs <markus.rohsto@gmail.com>
2021-02-04 11:20:55 +0100

    Autogen STDDEV samt snyggat till

--- ntp_diag.sh
+++ ntp_diag.sh
@@ -1,28 +1,71 @@
 #!/bin/bash
 
-INTERVAL=2
+# Tidsintervall mellan förfråningar.
+INTERVAL=1
+
+# Körtid i minuter
+DURATION=2
+
+# Tidsserver att ställa frågor mot.
 SERVER="192.36.143.130"
+
+# Fulständig sökväg till output fil. Om filen inte finns skapas den annars fylls befintlig fil på.
 OUT="/tmp/timediff1.csv"
+
+# Plockar ut aktuellt år eftersom ntpdate endast använder Amerikanskt datumformat utan år.
 YEAR=`date +"%Y"`
 
-#echo $YEAR
+let loops=($DURATION*60)/$INTERVAL
+let end=$loops+1
+#echo $loops
 
+
+
+# Funktion för att ställa fråga mot angiven tidsserver, samt trimma ner output till intressant data.
 get_ntp_time () {
     printf "$(date +%Y-%m-%d), $(date +%T:%N), " >> $OUT
-    #ntpdate -q $SERVER | grep -v "^server " | awk -vyear="$YEAR" -F " " '{ print $1,$2" year,",$3",",$8",",$10 }'
     ntpdate -q $SERVER | grep -v "^server " | awk  -vyr="$YEAR" -F " "  '{ print $1,$2,yr",",$3",",$8",",$10 }'
 }
 
+# Funktion för att skriva output till fil
 log_to_file () {
+    # Om filen saknas skapas den, och kollumnrubriker skrivs.
     if [ ! -f "$OUT" ]; then
-        #echo "$OUT does not exist."
         touch $OUT
-        echo "Local Date, Local Timestamp, NTP-date, NTP-time, server, offset" > $OUT
+        echo 'Local Date, Local Timestamp, NTP-date, NTP-time, server, offset, STD,"=STDEV(F2:F'$end')" ' > $OUT
+
+        
         get_ntp_time >> $OUT
+    # Om filen finns fylls den på med data
     else
-        #echo "$OUT exists.""
         get_ntp_time >> $OUT    
     fi
 }
 
-while true; do log_to_file; sleep $INTERVAL; done
+echo "Frågar "$SERVER" var "$INTERVAL" sekund."
+echo "Sparar till:" $OUT. 
+echo "Avbryt körning med Ctrl+C"
+echo ""
+
+# Oändlig loop som kör log_to_file med givet intervall.
+
+
+#while [ i -le 5 ]
+#do 
+#    log_to_file
+#    let i=i++
+#    sleep $INTERVAL
+
+#echo $i
+#done
+i=1
+while [ $i -le $loops ]
+do
+
+  echo -e '\e[1A\e[KFråga' $i'/'$loops
+  
+  #echo "Fråga" $i "av" $loops
+  log_to_file
+  sleep $INTERVAL
+  i=$(( $i + 1 ))
+done
